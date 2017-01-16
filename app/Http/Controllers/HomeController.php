53ee8fa483da6f267d0bb6f0926c7dc9d559ddb1
Your Name Here <psobhanlo@gmail.com>
2017-01-16 01:49:06 -0800

    take laravel

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -27,8 +27,9 @@ class HomeController extends Controller
     {
 
 
-     
-        $items = item::all();
+    $items= item::where('published', 1)->get();
+
+   
         return view('index',compact('items'));
 
     }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -63,7 +63,7 @@
 
 <li>
                   <div class="media wow fadeInDown"> <a href="pages/single_page.html" class="media-left"> <img alt="" src="images/post_img1.jpg"> </a>
-                    <div class="media-body"> <a href="pages/single_page.html" class="catg_title"> Aliquam malesuada diam eget turpis varius 1</a> </div>
+                    <div class="media-body"> <a href="pages/single_page.html" class="catg_title">{{$item->name}}</a> </div>
                   </div>
                 </li>
 
