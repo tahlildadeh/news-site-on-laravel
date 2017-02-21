43532ca61cf74778bff4c8bed554ccf7fe05b0e4
arghavankamyar <arghavankamyar@gmail.com>
2017-02-21 22:50:59 +0330

    Add files via upload

---
+++ app/nationalCode.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class nationalCode extends Model
+{
+    protected $fillable = ['first_name','last_name','national_code','gender'];
+}
